8b1a2e09952800defab2dab3891b3f06e028f128
Yash Bhongale <yashbhongale370@gmail.com>
2026-08-07 05:16:26 +0530

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,8 @@
+Scanner => Simple text scanner which allows system to scan a user input.
+For example this code allows system to get an integer input.
+-> Sacnner scanner = new Scanner(System.in);
+    int i = scanner.nextInt();
+
+import java.util.Scanner; <--needed to access the library.
+
+public class
