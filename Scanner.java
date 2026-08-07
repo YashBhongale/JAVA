6b0f93f71361dad7fd6676a9c21f6ac2d810fcf0
Yash Bhongale <yashbhongale370@gmail.com>
2026-08-07 05:40:19 +0530

    Implement main method for Scanner input example
    Added a main method to demonstrate Scanner usage for input.

--- Scanner.java
+++ Scanner.java
@@ -5,4 +5,28 @@ For example this code allows system to get an integer input.
 
 import java.util.Scanner; <--needed to access the library.
 
-public class
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        sc.close();
+
+        System.out.print("Your name is: " + name);
+    }
+}
+
+there are two different things:
+
+Scanner → A class (blueprint) provided by Java.
+sc → An object (variable/reference) of the Scanner class.
+
+Scanner	-> Class provided by Java to read input
+sc	-> Object/reference variable of the Scanner class
+new	-> Creates a new object
+Scanner(System.in)	-> Constructor that creates a Scanner to read keyboard input
+sc.nextLine()	-> Reads a full line of input
+sc.close()	-> Closes the Scanner
+
+We need need to close the scanner by .close comand cause if we do naot it can occupy unnecessary memory from system 
