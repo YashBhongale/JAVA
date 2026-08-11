ab44b9edbf354ff108802f170cf0ba9fa2a11765
Yash Bhongale <yashbhongale370@gmail.com>
2026-08-11 11:46:09 +0530

    Update Ternary_op.java

--- Ternary_op.java
+++ Ternary_op.java
@@ -7,8 +7,27 @@ public class Main{
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter your Marks: ");
     double marks = sc.nextDouble();
+    if(marks<0){
+      System.out.print("Please Enter a Positive value.");
+      break;    <-- yet to check?
+    }
     String result = (marks>35) ? "Pass" : "Fail";
     System.out.print(result);
+    sc.close();
+  }
+}
+
+//Even or Odd
+
+import java.util.Scanner;
+
+public class Main{
+  public static void main(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number to check if it is Even or Odd: ");
+    double num = sc.nextDouble();
+    String check = (num%2 == 0) ? "Even" : "Odd";
+    System.out.print(check);
     sc.close();
   }
 }
