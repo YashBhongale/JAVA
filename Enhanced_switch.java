dff38da8a395da22a8f4408f5961f0d60a0b3c46
Yash Bhongale <yashbhongale370@gmail.com>
2026-08-12 20:47:11 +0530

    Create Enhanced_switch.java

---
+++ Enhanced_switch.java
@@ -0,0 +1,10 @@
+Enhanced Switches are replacement to many if else statement and make it more readable. 
+
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    
+  }
+}
